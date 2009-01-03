1dcac64fe9f0ea1f6b22b91ff8d5657e858bb88e
James Brewer <james@jamesbrewer.io>
2009-01-02 21:38:53 -0800

    Use absolute paths for static and templates dirs.

--- thinkster_django_angular_boilerplate/settings.py
+++ thinkster_django_angular_boilerplate/settings.py
@@ -83,9 +83,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
-STATICFILES_DIRS = ('static',)
-TEMPLATE_DIRS = ('templates',)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
