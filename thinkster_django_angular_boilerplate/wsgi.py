c78088bd8b788f609f3c83dad1830a88b7da80d0
James Brewer <james@jamesbrewer.io>
2009-01-02 21:38:53 -0800

    Wrap get_wsgi_application() with Cling().

--- thinkster_django_angular_boilerplate/wsgi.py
+++ thinkster_django_angular_boilerplate/wsgi.py
@@ -11,4 +11,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thinkster_django_angular_boilerplate.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
