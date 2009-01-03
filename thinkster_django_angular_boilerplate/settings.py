f868969139d1801d23d6b1db8ed0098f03640517
James Brewer <james@jamesbrewer.io>
2009-01-02 21:38:53 -0800

    Use dj_database for database settings.

--- thinkster_django_angular_boilerplate/settings.py
+++ thinkster_django_angular_boilerplate/settings.py
@@ -57,11 +57,12 @@ WSGI_APPLICATION = 'thinkster_django_angular_boilerplate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+    )
 }
 
 # Internationalization
