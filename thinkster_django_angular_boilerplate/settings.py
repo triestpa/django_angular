689aeaaf376ac0b76df8f7d78b7ff7ab242fbede
James Brewer <james@jamesbrewer.io>
2009-01-02 21:38:53 -0800

    Look in os.environ for DEBUG. Default to True.

--- thinkster_django_angular_boilerplate/settings.py
+++ thinkster_django_angular_boilerplate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '$6(x*g_2g9l_*g8peb-@anl5^*8q!1w)k&e&2!i)t6$s8kia94'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', True)
 
 TEMPLATE_DEBUG = True
 
