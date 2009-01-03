c192ac3bad8e213ba4bacff8bb1479539f3503dc
James Brewer <james@jamesbrewer.io>
2009-01-02 21:38:53 -0800

    Add a couple of Heroku settings.

--- thinkster_django_angular_boilerplate/settings.py
+++ thinkster_django_angular_boilerplate/settings.py
@@ -107,3 +107,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     )
 }
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
