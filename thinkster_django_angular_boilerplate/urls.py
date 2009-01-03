63cd0be349204f8dc42109b15e1c491395d545a2
James Brewer <james@jamesbrewer.io>
2009-01-02 21:38:53 -0800

    Set up catch all route.

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
+from django.views.generic.base import TemplateView
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'thinkster_django_angular_boilerplate.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+    '',
 
-    url(r'^admin/', include(admin.site.urls)),
+    url('^.*$', TemplateView.as_view(template_name='index.html'), name='index'),
 )
