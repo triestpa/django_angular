bdbdc7112f2b892368f36c901ffa22b7892e5503
James Brewer <james@jamesbrewer.io>
2009-01-02 21:38:53 -0800

    Add settings for Django REST Framework.

--- thinkster_django_angular_boilerplate/settings.py
+++ thinkster_django_angular_boilerplate/settings.py
@@ -85,3 +85,9 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ('static',)
 TEMPLATE_DIRS = ('templates',)
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
